4ace5bf9bba936a546dcdb48c6cf212769dbb68d
douglas.dmc <douglasmarques37@gmail.com>
2023-09-04 15:57:33 -0300

    main.js

--- main.js
+++ main.js
@@ -1,16 +1,30 @@
-const produto1 = {
+const catalogo = [
+  {
+    id: 1,
     nome: "Casaco Branco",
     marca: "Zara",
     preco: 70,
-    nomeArquivoImage: "produto-1.jpg"
+    imagem: "product-1.jpg",
+    feminino: false
+  },
+  {
+    id: 2,
+    nome: "Sobretudo Azul Marinho",
+    marca: "Zara",
+    preco: 110,
+    imagem: "product-2.jpg",
+    feminino: true
+  },
+];
+
+for (const produtoCatalogo of catalogo) {
+  const cartaoProduto = `<div id="card-produto-1">
+    <img src="./assets/img/${produtoCatalogo.imagem}" alt="Produto 1 do magazine" style="height: 300px;">
+    <p>${produtoCatalogo.marca}</p>
+    <p>${produtoCatalogo.nome}</p>
+    <p>R$${produtoCatalogo.preco}</p>
+    <button>Adicionar</button>
+    </div>`;
+
+  document.getElementById("container-produto").innerHTML += cartaoProduto;
 }
-
-const cartaoProduto = `<div id="card-produto-1">
-<img src="./assets/img/${produto1.nomeArquivoImage}" alt="Produto 1 do magazine" style="height: 300px;">
-<p>${produto1.marca}</p>
-<p>${produto1.nome}</p>
-<p>R$${produto1.preco}</p>
-<button>Adicionar</button>
-</div>`
-
-document.getElementById("container-produto").innerHTML += cartaoProduto
